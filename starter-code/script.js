374d489fbdb097f076aff491569100b6bf2bb33f
Taylor <tayjoconn@gmail.com>
2021-06-18 07:32:24 -0700

    code-review ready
    removed test api

--- starter-code/script.js
+++ starter-code/script.js
@@ -12,43 +12,6 @@ hamburger.addEventListener('click', () => {
     icon.classList.toggle('fa-bars');
 });
 
-//**--NASA picture of the day API integration! --**//
-
-const url = 'https://api.nasa.gov/planetary/apod?api_key='
-const api_key = config.NASA_API_KEY
-
-
-const fetchNASAapi = async () =>{
-    try{
-        //create fetch for api using template literals calling from the defined variables above
-        const response = await fetch(`${url}${api_key}`);
-            //use .json() to parse JSON data
-            const data = await response.json();
-            console.log('NASA APOD data', data);
-            displayData(data);
-             //handle any errors - catches
-         } catch (error){
-                console.log(error);
-            }
-    }
-    fetchNASAapi();
-
-const displayData = data => {
-    document.getElementById('title_POTD').textContent = "Title:  "+data.title;
-    document.getElementById('date_POTD').textContent = "Date:  "+data.date;
-    document.getElementById('explanation_POTD').textContent = "Summary:  "+data.explanation;
-    document.getElementById('picture_POTD').src = data.hdurl;
-}
-
-const nasaButton = document.getElementById('Nasa_button');
-const picture_desc = document.getElementById('explanation_POTD');
-const nasaCloseDesc = document.getElementById('nasa_learnMore');
-
-nasaButton.addEventListener('click', () => {
-    picture_desc.classList.toggle('button_show');
-    nasaCloseDesc.classList.toggle('fa-angle-right');
-    nasaCloseDesc.classList.toggle('fa-times-circle');
-});
 
 
 const planetJSON = async () => {
